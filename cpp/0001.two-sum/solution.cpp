0461cb7d1a5b586b6e03a68a2bb3467fc49341ca
oportunitas <taibizzat165@gmail.com>
2026-08-20 16:12:56 +0700

    ✨ two-sum: add runtime-efficient option

--- cpp/0001.two-sum/solution.cpp
+++ cpp/0001.two-sum/solution.cpp
@@ -10,17 +10,33 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (auto i : views::iota(0uz, nums.size() - 1)) {
-			int num0 = nums[i];
-			for (auto [j, num1] : nums | views::drop(i + 1) | views::enumerate) {
-				// views::enumerate enumerates from 0, so we add (i + 1) to j to get 
-				// what j would be if the array was still intact
-				if (num0 + num1 == target) return {(int)(i), (int)((i + 1) + j)};
+	// runtime-efficient solution, but memory-heavy
+	vector<int> twoSum(vector<int>& nums, int target) {
+		// here the map is ordered as {value, index}
+		unordered_map<int, int> num_to_idx;
+        for (auto [i, num0] : nums | views::enumerate) {
+			if (auto it = num_to_idx.find((int)(target - num0)); it != num_to_idx.end()) {
+				auto [num1, j] = *it;
+				return {(int)i, (int)j};
+			} else {
+				num_to_idx[num0] = i;
 			}
 		}
 		return {};
     }
+
+	// // memory-efficient solution, but runtime-heavy
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     for (auto i : views::iota(0uz, nums.size() - 1)) {
+	// 		int num0 = nums[i];
+	// 		for (auto [j, num1] : nums | views::drop(i + 1) | views::enumerate) {
+	// 			// views::enumerate enumerates from 0, so we add (i + 1) to j to get 
+	// 			// what j would be if the array was still intact
+	// 			if (num0 + num1 == target) return {(int)(i), (int)((i + 1) + j)};
+	// 		}
+	// 	}
+	// 	return {};
+    // }
 };
 
 // @lc code=end
