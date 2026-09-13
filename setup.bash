9af5f73d9a347d9719314dcbbd4aa884a11ce9da
oportunitas <taibizzat165@gmail.com>
2026-09-13 18:26:40 +0700

    🧐 add valgrind analization

---
+++ setup.bash
@@ -0,0 +1,23 @@
+compile() {
+    local cur_dir="$(pwd)"
+    local id="$1"
+    echo "$id"
+    cd "$cur_dir/cpp/$id"*
+
+    #g++-16 -S -I .. ./solution.cpp -O2
+    g++-16 -O2 -I .. solution.cpp -o solution
+}
+
+analyze() {
+    local cur_dir="$(pwd)"
+    local id="$1"
+    echo "$id"
+    cd "$cur_dir/cpp/$id"*
+
+    #g++-16 -S -I .. ./solution.cpp -O2
+    g++-16 -O2 -I .. solution.cpp -o solution
+    valgrind --tool=callgrind ./solution
+    kcachegrind callgrind.out.*
+    
+    cd "$cur_dir"
+}
