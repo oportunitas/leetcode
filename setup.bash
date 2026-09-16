219499e471004ee3ec1ad2b336a25280215be64d
oportunitas <taibizzat165@gmail.com>
2026-09-16 13:46:16 +0700

    🧐 edit setup.bash

--- setup.bash
+++ setup.bash
@@ -15,7 +15,7 @@ analyze() {
     cd "$cur_dir/cpp/$id"*
 
     #g++-16 -S -I .. ./solution.cpp -O2
-    g++-16 -O2 -I .. solution.cpp -o solution
+    g++-16 -g -O2 -std=c++23 -I .. solution.cpp -o solution
     valgrind --tool=callgrind ./solution
     kcachegrind callgrind.out.*
     
